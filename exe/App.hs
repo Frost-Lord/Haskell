11501a7d0581266ebba11c047bebfbad33a6b594
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2022-12-14 16:58:32 +1100

    Update Haskell and install Cabal

---
+++ exe/App.hs
@@ -0,0 +1,23 @@
+-- Import the necessary modules
+import random
+
+-- Define a list of quotes to choose from
+quotes :: [String]
+quotes = [
+    "The only true wisdom is in knowing you know nothing. - Socrates",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. - Ralph Waldo Emerson",
+    "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. - Steve Jobs"
+  ]
+
+-- Define a function that chooses a random quote from the list
+chooseQuote :: IO String
+chooseQuote = randomRIO (0, length myList - 1)
+
+-- Define the main function
+main :: IO ()
+main = do
+    quote <- chooseQuote
+    let randomElement = quotes !! i
+    putStrLn randomElement
